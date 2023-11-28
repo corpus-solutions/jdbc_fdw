51aef09064b1937112c0e3dedf499531a17c177d
jzajic <jan.zajic@corpus.cz>
2023-11-28 16:42:01 +0100

    better error handling - send exception reason to client

--- jq.c
+++ jq.c
@@ -1433,8 +1433,7 @@ jq_get_exception()
 		exceptionMsg = (jstring) (*Jenv)->CallObjectMethod(Jenv, exc, exceptionMsgID);
 		exceptionString = jdbc_convert_string_to_cstring((jobject) exceptionMsg);
 		err_msg = pstrdup(exceptionString);
-		ereport(DEBUG3, (errmsg("%s", err_msg)));
-		ereport(ERROR, (errmsg("remote server returned an error")));
+		ereport(ERROR, (errmsg("Error in jdbc_fdw!\n%s", err_msg)));
 	}
 	return;
 }
